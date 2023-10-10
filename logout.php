df1410bd118394dcfe7184fb74df35cbd33e6b3e
Bariq Adyatma <53467656+bariqadyatam@users.noreply.github.com>
2023-10-10 18:00:20 +0700

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Menghapus semua variabel sesi
-session_destroy(); // Menghancurkan sesi
-header("location: index.php"); // Ganti dengan halaman login
-exit();
-?>
